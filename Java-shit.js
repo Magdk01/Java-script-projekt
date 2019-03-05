1a606434ba3a5a42633f799f211b0e020050bdf4
Mathias Auchenberg <mathiasauchenberg@mathiass-macbook-air.local>
2019-03-05 09:05:31 +0100

    testing in progress

---
+++ Java-shit.js
@@ -0,0 +1,12 @@
+    var stof = parseFloat(stof);
+    var konstant = parseFloat(0,0831);
+    var temperatur = parseFloat(temperatur);
+    var trykket = parseFloat(trykket);
+    var volume = parseFloat(volume);
+    var mar = parseFloat();
+
+function boyle(stof,volume){
+    return stof*volume=mar;
+}
+document.getElementById("").innerHTML = boyle();
+
